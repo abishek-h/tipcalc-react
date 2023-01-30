e40f557c963c0351c63a7975a9b8e0db21fa2cf5
Abishek H <abixshek@gmail.com>
2023-01-30 22:25:26 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calc />
     </div>
   );
-}
+};
+
+const Calc = () => {
+  return (
+    <body>
+      <div class="king-container">
+        <div class="title">Bill Calculator</div>
+        <div class="container-top">
+          <div class="heading">Bill Total</div>
+          <div>
+            <input type="text" placeholder="$0.00" id="totalbill" />
+          </div>
+          <div class="heading">Tip %</div>
+          <div>
+            <input type="text" placeholder="%0.0" id="tip-per" />
+          </div>
+        </div>
+        <div class="container-bottom">
+          <div class="bottom-left">
+            <div class="text">People</div>
+            <div class="operations">
+              <div class="button">
+                <button>+</button>
+              </div>
+              <div class="n-o-p" id="no-of-ppl">
+                1
+              </div>
+              <div class="button">
+                <button>-</button>
+              </div>
+            </div>
+          </div>
+          <div class="bottom-right">
+            <div class="text">Total Per Person</div>
+            <div class="amount-pp" id="amount-p-p">
+              $0.0
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
+  );
+};
 
 export default App;
